e20ae40cf5e151b492fc91ccc3ef1863186df74c
Yuki Ozawa <yuki_ozawa@esrij.com>
2018-07-05 15:06:04 +0900

    ローカル サーバー（ジオプロセシング サービス）のサンプルの追加

---
+++ src/desktop/LocalServer/Geoprocessing/LocalServer_GP/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.IO;
+
+using Esri.ArcGISRuntime.LocalServices;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Tasks.Geoprocessing;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+
+namespace LocalServer_GP
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private LocalGeoprocessingService localGpService;
+        private GraphicsOverlay resultOverlay;
+
+        private FeatureCollectionTable pointsTable;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Initialize();
+
+            this.Dispatcher.ShutdownStarted += ShutdownSample;
+            this.Unloaded += ShutdownSample;
+
+        }
+
+        private async void ShutdownSample(object sender, EventArgs e)
+        {
+            try
+            {
+                // アプリケーション終了時にローカル サーバーを停止
+                if (LocalServer.Instance.Status == LocalServerStatus.Started)
+                {
+                    await LocalServer.Instance.StopAsync();
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                return;
+            }
+        }
+
+        private async void Initialize()
+        {
+            // マップの背景地図と初期表示位置を設定
+            MyMapView.Map = new Map(BasemapType.Topographic, 35.68151, 139.76558, 14);
+
+            // ポイント格納用のフィーチャ コレクション テーブルの作成
+            pointsTable = new FeatureCollectionTable(null, GeometryType.Point, SpatialReferences.Wgs84);
+
+            // ポイント用のシンボルの作成
+            var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Colors.Red, 8);
+            // シンボルからレンダラーを作成し、フィーチャ コレクション テーブルのレンダラーに設定
+            pointsTable.Renderer = new SimpleRenderer(pointSymbol);
+
+            
+            // ポイントの作成
+            try
+            {
+                // 緯度経度のログを格納した CSV ファイルを開く
+                String csvPath = Directory.GetCurrentDirectory() + @"\SampleData\PointLog.csv";
+                using (var sr = new System.IO.StreamReader(csvPath))
+                {
+                    // CSVの行を繰り返し読み込む
+                    while (!sr.EndOfStream)
+                    {
+                        // 読む込んだ行をカンマ毎に分けて、緯度経度を取得する
+                        var line = sr.ReadLine();
+                        var values = line.Split(',');
+                        // 緯度経度を指定してポイントのフィーチャを作成する
+                        Feature pointFeature = pointsTable.CreateFeature();
+                        MapPoint point = new MapPoint(Double.Parse(values[1]), Double.Parse(values[0]), SpatialReferences.Wgs84);
+                        pointFeature.Geometry = point;
+                        // 作成したポイントをフィーチャ コレクション テーブルに追加
+                        await pointsTable.AddFeatureAsync(pointFeature);
+
+                    }
+                }
+            }
+
+            catch (System.Exception ex)
+            {
+                // CSV ファイルを開くのに失敗
+                MessageBox.Show(String.Format(ex.Message));
+            }
+
+
+            // フィーチャ コレクション テーブルからレイヤーを作成しマップに表示
+            FeatureCollection featuresCollection = new FeatureCollection();
+            featuresCollection.Tables.Add(pointsTable);
+            FeatureCollectionLayer collectionLayer = new FeatureCollectionLayer(featuresCollection);
+            MyMapView.Map.OperationalLayers.Add(collectionLayer);
+
+            // ジオプロセシング ツールを実行した結果のラインを表示するためのシンボルを作成
+            var outputLineSym = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Colors.Blue, 4);
+            var renderer = new SimpleRenderer(outputLineSym);
+
+            // 結果のラインを表示するグラフックス オーバレイを作成し、マップに追加
+            resultOverlay = new GraphicsOverlay();
+            resultOverlay.Renderer = renderer;
+            MyMapView.GraphicsOverlays.Add(resultOverlay);
+
+            try
+            {
+                // Local Server の起動をハンドリングするための StatusChanged イベントを設定
+                LocalServer.Instance.StatusChanged += ServerStatusChanged;
+                // Local Server のインスタンスを起動
+                await LocalServer.Instance.StartAsync();
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(String.Format(ex.Message));
+            }
+        }
+
+        private void ServerStatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Local Server の起動が成功したらツール実行ボタンを有効化する
+            if (e.Status == LocalServerStatus.Started)
+            {
+                StartBtn.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click_Start(object sender, RoutedEventArgs e)
+        {
+            // 実行結果表示用のグラフックス オーバレイに追加されているグラフィックスを消去
+            resultOverlay.Graphics.Clear();
+            // ローカル ジオプロセシング サービスを開始する処理
+            StartLocalGpService();
+            StartBtn.IsEnabled = false;
+        }
+
+        private async void StartLocalGpService()
+        {
+            // ジオプロセシング パッケージ ファイルのパスを取得
+            String gpkPath = Directory.GetCurrentDirectory() + @"\SampleData\PointsToLine.gpk";
+            //  パッケージ ファイルからローカル ジオプロセシング サービスを作成
+            localGpService = new LocalGeoprocessingService(gpkPath);
+
+            // ローカル ジオプロセシング サービスの起動をハンドリングするための StatusChanged イベントを設定
+            localGpService.StatusChanged += _localGpService_StatusChanged;
+
+            // ローカル ジオプロセシング サービスを起動
+            await localGpService.StartAsync();
+        }
+
+        private async void _localGpService_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // ジオプロセシング サービスの起動が成功したら、ジオプロセシングの処理を実行
+            if (e.Status == LocalServerStatus.Started)
+            {
+                // ジオプロセシング サービスに含まれているツール名を指定し、実行する URL を作成する
+                // "Points_To_Line" はジオプロセシング パッケージに指定したツール名
+                var gpSvcUrl = localGpService.Url.AbsoluteUri + "/Points_To_Line";
+
+                // URL を指定してジオプロセシング タスクを作成
+                GeoprocessingTask _bufferTask = await GeoprocessingTask.CreateAsync(new Uri(gpSvcUrl));
+
+                // ジオプロセシング タスクのパラメーターを設定
+                // パラメーター名はジオプロセシング ツールのパラメータ名と同様
+                // http://desktop.arcgis.com/ja/arcmap/latest/tools/data-management-toolbox/points-to-line.htm
+                GeoprocessingParameters myParameters = new GeoprocessingParameters(GeoprocessingExecutionType.AsynchronousSubmit);
+                myParameters.Inputs.Add("Input_Features", new GeoprocessingFeatures(pointsTable));
+
+                // パラメーターを指定しジオプロセシング タスクを実行する
+                // ジオプロセシング タスクのジョブを作成
+                var myGpJob = _bufferTask.CreateJob(myParameters);
+
+                try
+                {
+                    // ジョブ（解析）を実行し、結果を取得
+                    GeoprocessingResult myAnalysisResult = await myGpJob.GetResultAsync();
+                    // パラメータを指定して解析結果のフィーチャを取得
+                    GeoprocessingFeatures myResultFeatures = myAnalysisResult.Outputs["Output_Feature_Class"] as GeoprocessingFeatures;
+                    // フィーチャを解析結果表示用のグラフィック オーバレイに表示
+                    IFeatureSet myFeatures = myResultFeatures.Features;
+                    foreach (var myFeature in myFeatures)
+                    {
+                        resultOverlay.Graphics.Add(new Esri.ArcGISRuntime.UI.Graphic(myFeature.Geometry));
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    // ジョブが失敗したらエラー メッセージを表示
+                    if (myGpJob.Status == Esri.ArcGISRuntime.Tasks.JobStatus.Failed && myGpJob.Error != null)
+                        MessageBox.Show("ジオプロセシング処理の実行に失敗しました" + myGpJob.Error.Message);
+                    else
+                        MessageBox.Show("エラーが発生しました" + ex.ToString());
+                }
+                finally
+                {
+                    // 処理が完了したら、ローカル ジオプロセシング サービスを停止
+                    await localGpService.StopAsync();
+
+                }
+
+
+            }
+            else if (e.Status == LocalServerStatus.Failed)
+            {
+                MessageBox.Show("ジオプロセシング サービスの起動に失敗しました");
+
+            }
+            else if (e.Status == LocalServerStatus.Stopped)
+            {
+                MessageBox.Show("ジオプロセシング サービスが停止しました");
+                StartBtn.IsEnabled = true;
+            }
+        }
+
+       
+    }
+}
---
+++ src/desktop/LocalServer/Geoprocessing/README.md
@@ -0,0 +1,45 @@
+# サンプルの概要
+ArcGIS Runtime SDK for .NET（WPF API）のローカル サーバー機能を使用して、ローカル ジオプロセシング サービスを作成、実行する方法を紹介するサンプル アプリケーションです。
+このサンプルでは「ポイント → ライン (Points to Line)」ジオプロセシング ツールを使用します。
+
+![localserver-geoprocessing-dotnet](image/thumbnai.png)
+
+# 開発環境
+* ArcGIS Runtime SDK バージョン 100.2.1
+
+# ローカル サーバーを使用するための開発環境の準備
+
+ArcGIS Runtime SDK for .NET の動作要件は[動作環境ページ](https://www.esrij.com/products/arcgis-runtime-sdk-for-dotnet/environments)をご参照ください。
+
+ローカル サーバーを使用する場合は、上記要件に加えて以下の作業が必要です。
+
+1. Microsoft Visual C++ 再頒布可能パッケージのインストール
+ * Microsoft Visual C++ 2008 Redistributable Package (x86 または x64)
+ * Microsoft Visual C++ 2010 Redistributable Package (x86 または x64)
+ * Microsoft Visual C++ 2012 Redistributable Package (x86 または x64)
+ * Microsoft Visual C++ 2013 Redistributable Package (x86 または x64)
+1. [SDK のダウンロードページ](https://developers.arcgis.com/downloads/#arcgis-runtime-local-server)から ローカル サーバー用の SDK をダウンロードし、インストールします(ArcGIS for Developers の[開発者アカウント](http://esrijapan.github.io/arcgis-dev-resources/get-dev-account/)が必要です)。
+
+1. Visual Studio のプロジェクトで、Esri.ArcGISRuntime.WPF とは別に ローカル サーバー用の NuGet パッケージ（Esri.ArcGISRuntime.LocalServices ）も併せてインストールします（サンプル プロジェクトをそのまま実行する場合は、ビルド時に自動でインストールされます）。
+
+1. 最初にローカル サーバーを使用するプロジェクトをビルドすると、プロジェクト ディレクトリに XML 形式のマニフェスト ファイル（ArcGISLocalServer.AGSDeployment）が作成されます。このマニフェスト ファイルではアプリケーションに配置するローカル サーバーのコンポーネントを定義します。本サンプルを実行する場合は、以下の様に編集して "Geoprocessing" パッケージを有効にします（サンプル プロジェクトをそのまま実行する場合は、既に作成・設定済みのためこの作業は不要です）。
+```xml
+ <Package name="Geoprocessing" enabled="true">
+```
+
+ローカル サーバーを使用するための開発環境に関する詳細は [ArcGIS Runtime SDK for .NET (WPF) :Local Server](https://developers.arcgis.com/net/latest/wpf/guide/local-server.htm) をご参照ください。
+
+
+
+# ジオプロセシング パッケージの作成
+ローカル ジオプロセシング サービスを作成するには、事前にジオプロセシング パッケージ（.gpk）を作成する必要があります。
+サンプル コードでは、事前に作成済みの PointsToLine.gpk を使用していますが、自身で作成する場合は以下の手順で作成してください。
+
+1. ArcMap を起動します
+1. ArcMap で、「ポイント → ライン (Points to Line)」ジオプロセシング ツールを実行します
+1. ジオプロセシングの [結果] ウィンドウを開き、実行したツールを右クリックし、[共有] -> [ジオプロセシング パッケージ] を選択します
+1. 「パッケージをファイルに保存」、「スキーマのみパッケージ」、「ArcGIS Runtime のサポート」をチェックします
+※「ArcGIS Runtime のサポート」のチェックが表示されていない場合は、メニューの [カスタマイズ] -> [ArcMap オプション] -> [共有] で、「ArcGIS Runtime ツールの有効化」をチェックします
+1. 「ポイント → ライン (Points to Line)」のタブを選択して、名前を「Points_To_Line」に変更します
+1. [入力フィーチャ] パラメーターのタブを選択し、入力モードを「ユーザー定義値」に設定します
+1. 右上の [共有] ボタンをクリックして、.gpk ファイルを保存します
